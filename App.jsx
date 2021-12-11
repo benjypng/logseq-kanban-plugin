26bdba96e23d443d8bd048b70f583d97f14555a9
hkgnp <ben@pngs.cc>
2021-12-11 12:05:05 +0800

    added waiting dialogue

--- App.jsx
+++ App.jsx
@@ -28,7 +28,24 @@ const App = (props) => {
   // Prepare to draw board
   const [kanbanBoard] = useState(prepareDataForKanban());
 
-  return <Board>{kanbanBoard}</Board>;
+  console.log(kanbanBoard);
+
+  if (kanbanBoard.columns.length === 0) {
+    return (
+      <div
+        style={{
+          border: '1px solid white',
+          padding: '0.5em',
+          marginTop: '-2rem',
+        }}
+      >
+        Waiting for data to be rendered... Start by creating a child block
+        below, and adding your data below it!
+      </div>
+    );
+  } else {
+    return <Board>{kanbanBoard}</Board>;
+  }
 };
 
 export default App;
