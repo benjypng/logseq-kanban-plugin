3f83738644fd8af4a17b5ee7ac013dd4f6af88ef
hkgnp <ben@pngs.cc>
2021-12-15 11:52:15 +0800

    separated out css into another file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '@logseq/libs';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './App';
+import kanbanCss from './kanban.js';
 
 const main = async () => {
   console.log('Kanban plugin loaded');
@@ -20,10 +21,9 @@ const main = async () => {
   });
 
   logseq.App.onMacroRendererSlotted(async ({ slot, payload }) => {
+    console.log(logseq.settings);
     // Provide style for kanban board
-    logseq.provideStyle(`
-    .react-kanban-board{padding:2px;margin:0;color:#000}.react-kanban-card{border-radius:3px;background-color:#fff;padding:10px;margin-bottom:7px}.react-kanban-card,.react-kanban-card-skeleton{box-sizing:border-box;max-width:250px;min-width:250px}.react-kanban-card:hover{box-shadow:-5px 10px 15px #aaaaaa;}.react-kanban-card__description{padding-top:0}.react-kanban-card__title{display:none;padding:0;margin:0}.react-kanban-column{white-space:normal;max-width:290px;min-width:290px;padding:15px;border-radius:2px;background-color:#eee;margin:5px}.react-kanban-column input:focus{outline:0}.react-kanban-column-header{padding-bottom:10px;font-weight:700;font-size:120%;}.react-kanban-column:hover{transform:translateY(-8px);transition:all 0.1s;}
-    `);
+    logseq.provideStyle(`${kanbanCss()}`);
 
     // Get uuid of payload so that child blocks can be retrieved for the board
     const uuid = payload.uuid;
---
+++ src/kanban.js
@@ -0,0 +1,55 @@
+const kanbanCss = () => {
+  return `
+  .react-kanban-board {
+    padding: 2px;
+    margin: 0;
+    color: #000;
+  }
+  .react-kanban-card {
+    border-radius: 3px;
+    background-color: #fff;
+    padding: 10px;
+    margin-bottom: 7px;
+  }
+  .react-kanban-card,
+  .react-kanban-card-skeleton {
+    box-sizing: border-box;
+    max-width: 250px;
+    min-width: 250px;
+  }
+  .react-kanban-card:hover {
+    box-shadow: -5px 10px 15px #aaaaaa;
+  }
+  .react-kanban-card__description {
+    padding-top: 0;
+  }
+  .react-kanban-card__title {
+    display: none;
+    padding: 0;
+    margin: 0;
+  }
+  .react-kanban-column {
+    white-space: normal;
+    max-width: 290px;
+    min-width: 290px;
+    padding: 15px;
+    border-radius: 2px;
+    background-color: #000;
+    margin: 5px;
+  }
+  .react-kanban-column input:focus {
+    outline: 0;
+  }
+  .react-kanban-column-header {
+    padding-bottom: 10px;
+    font-weight: 700;
+    font-size: 120%;
+    color: #fff;
+  }
+  .react-kanban-column:hover {
+    transform: translateY(-8px);
+    transition: all 0.1s;
+  }`;
+};
+
+export default kanbanCss;
