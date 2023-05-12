539b0a14680c9b0e8125578f002e0244ab0ee05b
Matthew Scheffel <matt@dataheck.com>
2023-05-12 18:47:18 -0300

    Platform-independent build (scaffolding)

---
+++ build.js
@@ -0,0 +1,14 @@
+// https://stackoverflow.com/a/45082999/2205935
+
+var exec = require('child_process').exec;
+var os = require('os');
+
+function echo(_error, stdout, _stderr) { console.log(stdout) }
+
+// Run command depending on the OS
+if (os.type() === 'Linux') 
+    exec("rm -R dist && echo 'dist folder removed'; parcel build --no-source-maps src/index.html --public-url ./", echo); 
+else if (os.type() === 'Windows_NT') 
+    exec("echo hello", echo);
+else
+    throw new Error("Unsupported OS found: " + type());
