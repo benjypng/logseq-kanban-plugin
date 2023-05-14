b0c8e5a83041b1065e9275193c816b1db08f0007
Matthew Scheffel <matt@dataheck.com>
2023-05-14 13:23:36 -0300

    Separate clean from windows build
    required for first run when `dist` doesn't exist

--- build.js
+++ build.js
@@ -8,7 +8,8 @@ function echo(_error, stdout, _stderr) { console.log(stdout) }
 // Run command depending on the OS
 if (os.type() === 'Linux') 
     exec("rm -R dist && echo 'dist folder removed'; parcel build --no-source-maps src/index.html --public-url ./", echo); 
-else if (os.type() === 'Windows_NT') 
-    exec("rmdir /S /Q dist && npx parcel build --no-source-maps src/index.html --public-url ./", echo);
-else
+else if (os.type() === 'Windows_NT') {
+    exec("rmdir /S /Q dist", echo);
+    exec("npx parcel build --no-source-maps src/index.html --public-url ./", echo);
+} else
     throw new Error("Unsupported OS found: " + type());
