80f82b033f90282601e1a29e39638a8d1dbc300f
hkgnp <ben@pngs.cc>
2023-12-01 23:46:05 +0800

    feat: refactor to handle inline stuff

--- src/libs/process-content/handle-link.tsx
+++ src/libs/process-content/handle-link.tsx
@@ -3,11 +3,9 @@ import reactStringReplace from "react-string-replace";
 
 export const handleLink = (str: ReactNode[]): ReactNode[] => {
   const rxLinkRef = /(https:\/\/[\w.-]+(?:\/[\w.-]*)*)/gi;
-  return reactStringReplace(str, rxLinkRef, (match, i) => {
-    return (
-      <a key={i} href={match} target="_blank" className="external-link">
-        {match}
-      </a>
-    );
-  });
+  return reactStringReplace(str, rxLinkRef, (match, i) => (
+    <a key={i} href={match} className="external-link" target="_blank">
+      {match}
+    </a>
+  ));
 };
