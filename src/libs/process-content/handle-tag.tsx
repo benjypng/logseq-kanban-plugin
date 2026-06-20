a544a1e9f46562572136a6189c8afdbb24067b71
Matus Horvath <matus.horvath@sap.com>
2026-06-20 10:36:09 +0200

    Fix parsing of multi-word tags
    One issue was that the regex did not enforce that an opening [[ has to be followed by a closing ]]. Another issue was that a tag without [[]] was allowed to be multi-word.
    
    https://github.com/benjypng/logseq-kanban-plugin/issues/44

--- src/libs/process-content/handle-tag.tsx
+++ src/libs/process-content/handle-tag.tsx
@@ -1,30 +1,43 @@
 import { ReactNode } from 'react'
 import reactStringReplace from 'react-string-replace'
 
+const processMatchedTag = (
+  str: ReactNode[] | string,
+  match: string,
+  index: number
+): ReactNode => {
+  if (match === '#') return str;
+  if (match === 'A' || match === 'B' || match === 'C') {
+    return (
+      <a key={match + index} href={`logseq://graph/${name}?page=${match}`}>
+        {match}
+      </a>
+    );
+  } else {
+    return (
+      <div key={match + index}>
+        <a
+          href={`logseq://graph/${name}?page=${match}`}
+          className="kanban-tag"
+        >
+          {match}
+        </a>
+      </div>
+    );
+  }
+}
+
 export const handleTag = (
   str: ReactNode[] | string,
   name: string,
 ): ReactNode[] | string => {
-  const rxTagRef = /#(?:\[\[)?(\w+(?:\s+\w+)*)(?:\]\])?/g
-  return reactStringReplace(str, rxTagRef, (match, i) => {
-    if (match === '#') return str
-    if (match === 'A' || match === 'B' || match === 'C') {
-      return (
-        <a key={match + i} href={`logseq://graph/${name}?page=${match}`}>
-          {match}
-        </a>
-      )
-    } else {
-      return (
-        <div key={match + i}>
-          <a
-            href={`logseq://graph/${name}?page=${match}`}
-            className="kanban-tag"
-          >
-            {match}
-          </a>
-        </div>
-      )
-    }
-  })
+  const rxNoBraceTagRef = /#(\w+)/g;
+  str = reactStringReplace(str, rxNoBraceTagRef,
+    (match, index) => processMatchedTag(str, match, index));
+
+  const rxBraceTagRef = /#\[\[(\w+(?:\s+\w+)*)\]\]/g;
+  str = reactStringReplace(str, rxBraceTagRef,
+    (match, index) => processMatchedTag(str, match, index));
+
+  return str;
 }
