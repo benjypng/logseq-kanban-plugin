21086139eb44bc5f097eb07718900799f6876112
Matus Horvath <matus.horvath@sap.com>
2026-06-20 14:15:04 +0200

    Improved tag handling
    Correctly process international characters, better match the logseq/mldoc tag parsing algorithm

--- src/libs/process-content/handle-tag.tsx
+++ src/libs/process-content/handle-tag.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from 'react'
 import reactStringReplace from 'react-string-replace'
 
+// This code tries to approximate how logseq itself parses tags:
+// - https://github.com/logseq/mldoc/blob/master/lib/syntax/inline.ml#L1296
+// - https://github.com/logseq/mldoc/blob/master/lib/syntax/extended/hash_tag.ml
+// - https://github.com/logseq/mldoc/blob/master/lib/parsers.ml
+
 const processMatchedTag = (
-  str: ReactNode[] | string,
+  name: string,
   match: string,
-  index: number
+  index: number,
 ): ReactNode => {
-  if (match === '#') return str;
   if (match === 'A' || match === 'B' || match === 'C') {
     return (
       <a key={match + index} href={`logseq://graph/${name}?page=${match}`}>
@@ -31,13 +35,15 @@ export const handleTag = (
   str: ReactNode[] | string,
   name: string,
 ): ReactNode[] | string => {
-  const rxNoBraceTagRef = /#(\w+)/g;
-  str = reactStringReplace(str, rxNoBraceTagRef,
-    (match, index) => processMatchedTag(str, match, index));
+  // With brackets: [[ followed by any characters up to the next ]]
+  const rxBracketTagRef = /#\[\[((?:[^\]]|\][^\]])*)\]\]/gu;
+  str = reactStringReplace(str, rxBracketTagRef,
+    (match, index) => processMatchedTag(name, match, index));
 
-  const rxBraceTagRef = /#\[\[(\w+(?:\s+\w+)*)\]\]/g;
-  str = reactStringReplace(str, rxBraceTagRef,
-    (match, index) => processMatchedTag(str, match, index));
+  // Without brackets: Any characters other than tag_delims, space_chars, eol_chars (see logseq/mldoc sources)
+  const rxNoBracketTagRef = /#([^,;.!?'":# \t\r\n]+)/gu;
+  str = reactStringReplace(str, rxNoBracketTagRef,
+    (match, index) => processMatchedTag(name, match, index));
 
   return str;
 }
